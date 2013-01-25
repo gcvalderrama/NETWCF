4997292d0d808649f83400802324fe236860888d
Gregory Cesar Valderrama Vilca <gcvalderrama@hotmail.com>
2013-01-25 15:55:12 -0500

    add

---
+++ DEMO01/Compartir/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Compartir
+{
+    [DataContract]
+    public class LogMessage
+    {
+        [DataMember]
+        public string text {get;set;}
+        [DataMember]
+        public DateTime date {get;set;}
+    }
+
+    [ServiceContract]
+    public interface ILog
+    {
+        [OperationContract]
+        void LogMessage(string message);
+        [OperationContract]
+        void LogMessageObject(LogMessage message);
+        
+
+    }
+}
---
+++ DEMO01/Servidor/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Servidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
