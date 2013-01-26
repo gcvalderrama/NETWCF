165ce099c1681bb01196144d21d8ad32844fc0c6
Gregory Cesar Valderrama Vilca <gcvalderrama@hotmail.com>
2013-01-26 10:21:25 -0500

    demo

---
+++ DEMO01/LogWinService/LogService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace LogWinService
+{
+    public class LogService : Compartir.ILog
+    {
+        public void LogMessage(string message)
+        {
+            File.AppendAllText(@"C:\NETWCF\NETWCF\DEMO01\LogWinService\bin\Debug\salida.txt", message);
+        }
+
+        public void LogMessageObject(Compartir.LogMessage message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DEMO01/LogWinService/Service1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.ServiceModel;
+
+namespace LogWinService
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+        protected ServiceHost host;  
+        protected override void OnStart(string[] args)
+        {
+            host = new ServiceHost(typeof(LogService));
+            host.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (host != null) host.Close();  
+        }
+    }
+}
